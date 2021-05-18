baf9f60516f53bdc6016d535462c8d2f0ca88312
paulwesinger <pw@mei-baeck.com>
2021-05-18 21:02:21 +0200

    start new project

---
+++ main.cpp
@@ -0,0 +1,28 @@
+
+#include "engine/engine.h"
+
+
+int main(int argc,char ** argv)
+{
+
+
+    CEngine * init = new CEngine("Engine started");
+
+    if ( ! init ) {
+        printf( "Failed to Create Object !!! \n Aborting ......");
+        return 1;
+    }
+
+    if ( init->InitSDL2()) {
+
+        init->Run();
+
+    }
+    else {
+        printf(" Init SDL2 failed !! \n aborting ....");
+        return 2;
+    }
+    init->Done();
+
+    delete init;
+}
